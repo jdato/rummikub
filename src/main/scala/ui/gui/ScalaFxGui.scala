bff3aeef870a5b771f023ab891eef02ea52f93ac
Johannes Dato <jodato@htwg-konstanz.de>
2017-12-01 17:04:33 +0100

    [ScalaFx] Added number of players and disabled buttons

--- src/main/scala/ui/gui/ScalaFxGui.scala
+++ src/main/scala/ui/gui/ScalaFxGui.scala
@@ -4,10 +4,11 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.{Insets, Orientation}
 import scalafx.scene.Scene
-import scalafx.scene.control.{Button, ScrollPane, SplitPane, ToolBar}
+import scalafx.scene.control._
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.layout._
 import scalafx.scene.text.{Font, Text}
+import javafx.scene.control.{ToggleButton => JfxToggleBtn}
 
 object ScalaFxGui extends JFXApp {
 
@@ -22,6 +23,7 @@ object ScalaFxGui extends JFXApp {
     id = "playButton"
     text = "Play!"
     font = Font.apply("Arial Black", 20)
+    disable = true
   }
   val quitButton = new Button {
     minWidth = 120
@@ -29,6 +31,7 @@ object ScalaFxGui extends JFXApp {
     id = "quitButton"
     text = "Quit Game"
     font = Font.apply("Arial Black", 20)
+    disable = true
   }
   val playingfieldPane = new ScrollPane {
     vgrow = Priority.Always
@@ -70,6 +73,49 @@ object ScalaFxGui extends JFXApp {
     }
   }
 
+  val tog = new ToggleGroup {
+    selectedToggle.onChange(
+      (_, oldValue, newValue) => {
+        startButton.text = "Play! (" + newValue.asInstanceOf[JfxToggleBtn].getText + ")"
+        startButton.disable = false
+      }
+    )
+  }
+
+  val toggleGroup = new HBox {
+    margin = Insets.apply(0, 0, 0, 12)
+    spacing = 2
+    children = List(
+      new ToggleButton {
+        minWidth = 50
+        text = "2"
+        toggleGroup = tog
+      },
+      new ToggleButton {
+        minWidth = 50
+        text = "3"
+        toggleGroup = tog
+      },
+      new ToggleButton {
+        minWidth = 50
+        text = "4"
+        toggleGroup = tog
+      })
+  }
+
+  val playerSelect = new VBox {
+    margin = Insets.apply(10, 0, 0, 0)
+    children = List(
+    new Label {
+      text = "Number of players:"
+      minWidth = 180
+      style = "-fx-font-size: 1.5em"
+    },
+    toggleGroup
+  )}
+
+  val toggleLabel = new TextField
+
   stage = new PrimaryStage {
     title = "ScalaFX Rummikub Game"
     scene = new Scene(1020, 700) {
@@ -92,8 +138,10 @@ object ScalaFxGui extends JFXApp {
                 margin = Insets(0, 0, 0, 10)
               },
               new Region {
-                minWidth = 200
+                minWidth = 20
               },
+              playerSelect
+              ,
               startButton
               ,
               quitButton
