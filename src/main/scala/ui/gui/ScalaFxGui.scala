130aace5921e955d1d13850819f16d64eba6656c
Johannes Dato <jodato@htwg-konstanz.de>
2017-12-01 16:17:49 +0100

    [ScalaFx] Added the game window including start and quit buttons and the three panes for the gameplay

--- src/main/scala/ui/gui/ScalaFxGui.scala
+++ src/main/scala/ui/gui/ScalaFxGui.scala
@@ -2,47 +2,110 @@ package ui.gui
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Orientation}
 import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint.{Color, LinearGradient, Stops}
-import scalafx.scene.text.Text
+import scalafx.scene.control.{Button, ScrollPane, SplitPane, ToolBar}
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.layout._
+import scalafx.scene.text.{Font, Text}
 
 object ScalaFxGui extends JFXApp {
 
-  stage = new PrimaryStage {
-    //    initStyle(StageStyle.Unified)
-    title = "ScalaFX Hello World"
-    scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          },
-          new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
-            }
-          }
-        )
-      }
-    }
+  lazy val splitPane = new SplitPane {
+    orientation = new Orientation(Orientation.Vertical)
+    id = "gamePane"
+    items.addAll(playingfieldPane, possibleSetsPane, rackPane)
+  }
+  val startButton = new Button {
+    minWidth = 120
+    minHeight = 66
+    id = "playButton"
+    text = "Play!"
+    font = Font.apply("Arial Black", 20)
+  }
+  val quitButton = new Button {
+    minWidth = 120
+    minHeight = 66
+    id = "quitButton"
+    text = "Quit Game"
+    font = Font.apply("Arial Black", 20)
+  }
+  val playingfieldPane = new ScrollPane {
+    vgrow = Priority.Always
+    hgrow = Priority.Always
+    fitToWidth = true
+    minHeight = 200
+    id = "playingfield-pane"
 
+    content = new VBox {
+      vgrow = Priority.Always
+      hgrow = Priority.Always
+      children = new Text("Playing Field Pane.")
+    }
+  }
+  val possibleSetsPane = new ScrollPane {
+    vgrow = Priority.Always
+    hgrow = Priority.Always
+    fitToWidth = true
+    minHeight = 200
+    id = "possiblesets-pane"
+
+    content = new VBox {
+      vgrow = Priority.Always
+      hgrow = Priority.Always
+      children = new Text("Possible Sets Pane.")
+    }
+  }
+  val rackPane = new ScrollPane {
+    vgrow = Priority.Always
+    hgrow = Priority.Always
+    fitToWidth = true
+    minHeight = 200
+    id = "rack-pane"
+
+    content = new VBox {
+      vgrow = Priority.Always
+      hgrow = Priority.Always
+      children = new Text("Rack Pane.")
+    }
+  }
+
+  stage = new PrimaryStage {
+    title = "ScalaFX Rummikub Game"
+    scene = new Scene(1020, 700) {
+      //fill = Color.rgb(38, 38, 38)
+
+      root = new BorderPane {
+        top = new VBox {
+          vgrow = Priority.Always
+          hgrow = Priority.Always
+
+          // Toolbar oben mit new button
+          children = new ToolBar {
+            prefHeight = 76
+            maxHeight = 76
+            id = "mainToolBar"
+            content = List(
+              new ImageView {
+                image = new Image(
+                  this.getClass.getResourceAsStream("/images/logo.png"))
+                margin = Insets(0, 0, 0, 10)
+              },
+              new Region {
+                minWidth = 200
+              },
+              startButton
+              ,
+              quitButton
+            )
+          }
+        }
+
+        center = new BorderPane {
+          center = splitPane
+        }
+
+      } // End of root BorderPane
+    }
   }
 }
